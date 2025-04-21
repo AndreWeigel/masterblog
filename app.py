755073985ab3f638ff1aa46e7c8df12de3945977
andreweigel <aweigel.2000@gmail.com>
2025-04-21 11:50:31 -0300

    show blog posts in index.html

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from blog_manager import BlogManager
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    blog = BlogManager()
+    blog_posts = blog.get_all_posts()
+    return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
